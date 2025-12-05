4667bd8cc74a5fc4e1dba9bcb6eadd4acc4a29c7
EdinsonRC <120270923+EdinsonRC@users.noreply.github.com>
2025-12-05 00:31:46 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+
+    const togglePassword = document.querySelector('#togglePassword');
+    const password = document.querySelector('#password');
+    
+    if (togglePassword && password) {
+        togglePassword.addEventListener('click', function () {
+            const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+            password.setAttribute('type', type);
+            this.classList.toggle('bi-eye');
+            this.classList.toggle('bi-eye-slash');
+        });
+    }
+
+
+    const forms = document.querySelectorAll('.needs-validation')
+
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+        }, false)
+    });
+});
